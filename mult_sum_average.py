dbf041443d7d9e9d6142084db766179e17c472d6
Jason <jason.w.switzer@gmail.com>
2017-07-06 15:36:33 -0700

    completed mult sum and avg asg

--- mult_sum_average.py
+++ mult_sum_average.py
@@ -1,8 +1,17 @@
-#multiples
+#multiples part 1
+for count in range(1,1001,2):
+    print count
 
-
+#multiples part 2
+for count in range(5,1000000,5):
+    print count
 
 #sum list
-
+a = [1, 2, 5, 10, 255, 3]
+sum = 0
+for i in a:
+    sum += i
+print sum
 
 #average list
+print sum/len(a)
