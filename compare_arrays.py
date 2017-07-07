53d6a1a7c23e8ff949eff9225d7fef168ce783a0
Jason <jason.w.switzer@gmail.com>
2017-07-06 21:31:09 -0700

    completed compare arrays asg

--- compare_arrays.py
+++ compare_arrays.py
@@ -1,11 +1,11 @@
-#list_one = [1,2,5,6,2]
-#list_two = [1,2,5,6,2]
+list_one = [1,2,5,6,2]
+list_two = [1,2,5,6,2]
 
 #list_one = [1,2,5,6,5]
 #list_two = [1,2,5,6,5,3]
 
-list_one = [1,2,5,6,5,16]
-list_two = [1,2,5,6,5]
+#list_one = [1,2,5,6,5,16]
+#list_two = [1,2,5,6,5]
 
 #list_one = ['celery','carrots','bread','milk']
 #list_two = ['celery','carrots','bread','cream']
